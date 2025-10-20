710daf54e9bfba2acad1923d7782e6c299c3e8cf
Julian Millen <julimillen@gmail.com>
2025-10-20 11:22:33 -0300

    Formularios reactivos

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -12,14 +15,33 @@ export class LoginComponent implements OnInit {
 
   loginForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private router:Router) {}
 
   
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: ['julimillen@gmail.com'],
-      password: [''],
+      email: ['julimillen@gmail.com', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
     });
       
+  }
+
+  get email(){
+    return this.loginForm.get('email');
+  }
+
+  get password(){
+    return this.loginForm.get('password');
+  }
+
+  login(){
+    if(this.loginForm.valid){
+      console.log("Llamar al servicio de login");
+      this.router.navigateByUrl('/inicio');
+      this.loginForm.reset();
+    }else{
+      this.loginForm.markAllAsTouched();
+      alert("Error");
+    }
   }
 }
